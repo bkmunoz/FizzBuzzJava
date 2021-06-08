449e4222a9d84c576afe49938750d3c16f63774d
bkmunoz <82331185+bkmunoz@users.noreply.github.com>
2021-06-08 17:01:27 -0500

    Initial Upload

---
+++ FizzBuzzTest.java
@@ -0,0 +1,8 @@
+public class FizzBuzzTest {
+public static void main(String[] args){
+    FizzBuzz.fizzBuzz(3);
+    FizzBuzz.fizzBuzz(5);
+    FizzBuzz.fizzBuzz(15);
+    FizzBuzz.fizzBuzz(2);
+    }
+}
